5a902fad5069c7a47e307e71e2ac6654ad7daadd
breduardo <brunoed.0925@gmail.com>
2023-01-12 11:24:24 -0300

    Att

--- src/Object.js
+++ src/Object.js
@@ -2,4 +2,8 @@ module.exports = {
   GetKeyByValue: (object, value) => {
     return Object.keys(object).find((key) => object[key] === value);
   },
+
+  MergeObjects: (obj1, obj2) => {
+    return Object.assign(obj1, obj2);
+  },
 };
--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,20 @@
-const { Format } = require("../index");
-
+const { Format, Object } = require("../index");
 // Unit Test NodeJS
 
 test("OnlyNumbers", () => {
   const result = Format.OnlyNumbers("Bruno1234Teste");
   expect(result).toEqual("1234");
+});
+
+test("GetKeyByValue", () => {
+  const result = Object.GetKeyByValue({ 1: "Bruno", 2: "Teste" }, "Bruno");
+  expect(result).toEqual("1");
+});
+
+test("MergeObjects", () => {
+  const result = Object.MergeObjects(
+    { 1: "Bruno", 2: "Teste" },
+    { 3: "Teste" }
+  );
+  expect(result).toEqual({ 1: "Bruno", 2: "Teste", 3: "Teste" });
 });
