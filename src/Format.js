dfcd51f5d6dd78c0759693901d82755fe80e93e2
breduardo <brunoed.0925@gmail.com>
2023-05-02 22:46:02 -0300

    New formats

--- src/Format.js
+++ src/Format.js
@@ -2,4 +2,19 @@ module.exports = {
   OnlyNumbers: (valor) => {
     return valor.replace(/\D/g, "");
   },
+  // Float To Real
+  MoneyToReal: (valor) => {
+    return valor.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+  },
+  RemoverSpecialChars: (valor) => {
+    return valor
+      .normalize("NFD")
+      .replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, "");
+  },
+  RemoveNumbers: (valor) => {
+    return valor.replace(/[0-9]/g, "");
+  },
 };
